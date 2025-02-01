6d137cae23d5432e7577b6c3454bd258dddd7e2f
변상현 <bsh604080@gmail.com>
2025-02-02 00:02:18 +0900

    chore: url노출 부분삭제

--- src/page/AuctionItemPage.js
+++ src/page/AuctionItemPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from 'styled-components';
 import Footer from '../component/Footer'
 import eximg0 from '../asset/image/샌즈.jpg'
@@ -13,12 +13,27 @@ import {ReactComponent as Eye} from "../asset/svgs/eye.svg";
 import BackIcon from '../component/icons/BackIcon';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from "swiper/modules";
-import formatNumber from '../util/formatNumber';
+import Modal from 'react-modal';
+import {ReactComponent as Cancle} from "../asset/svgs/Cancle.svg"
+
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 
  
 function Itempage() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+  const images = [eximg, eximg2, eximg3];
+
+  const openModal = (index) => {
+    setCurrentImgIndex(index);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
 
 
     return (
@@ -35,9 +50,8 @@ function Itempage() {
                         <MoreVertIcon/>
                     </EtcIcon>
 
-                    <SellingIcon>
-                        <Selling/>
-                    </SellingIcon>
+                    <SellingIcon/>
+                    
                     
                     <StyledSwiper
                     pagination={{
@@ -45,9 +59,11 @@ function Itempage() {
                     }}
                     modules={[Pagination]}
                     >
-                    <SwiperSlide>  <PageImg src={ eximg } alt=""/>  </SwiperSlide>
-                    <SwiperSlide>  <PageImg src={ eximg2 } alt=""/>  </SwiperSlide>
-                    <SwiperSlide>  <PageImg src={ eximg3 } alt=""/>  </SwiperSlide>
+                    {images.map((img, index) => (
+                      <SwiperSlide key={index} onClick={() => openModal(index)}>  
+                        <PageImg src={img} alt={`Slide ${index + 1}`} />
+                      </SwiperSlide>
+                    ))}
                     </StyledSwiper>
                 
                 </PageImgBox>
@@ -127,6 +143,35 @@ function Itempage() {
             
             </PageMain>
             <Footer/>
+            <StyledModal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Image Modal"
+                    ariaHideApp={false}
+                    style={{
+                      overlay: { zIndex: 100 } 
+                    }}
+                   
+                  >
+                    <ModalContent>
+                      <CloseButton onClick={closeModal}>Close</CloseButton>
+                      <Swiper
+                        pagination={{
+                          dynamicBullets: true,
+                        }}
+                        modules={[Pagination]}
+                        initialSlide={currentImgIndex}
+                      >
+                        {images.map((img, index) => (
+                          <SwiperSlide key={index}>
+                            <ModalImgBox>
+                              <ModalImg src={img} alt={`Modal Slide ${index + 1}`} />
+                            </ModalImgBox>
+                          </SwiperSlide>
+                        ))}
+                      </Swiper>
+                    </ModalContent>
+                  </StyledModal>
         </PageStyle>
     )
 }
@@ -134,10 +179,10 @@ function Itempage() {
 export default Itempage
 
 const PageStyle=styled.div`
-  height: 100vh;
+  height: 100dvh;
   max-width: 480px;
   margin: 0 auto;
-  border: 1px solid #ddd; 
+
   background-color: #1C1C1E;
 `
 const PageMain=styled.div`
@@ -176,7 +221,7 @@ const EtcIcon=styled.svg`
   height: 12%;
   z-index: 2;
 `
-const SellingIcon = styled.svg`
+const SellingIcon = styled(Selling)`
   position: absolute;
   bottom: 10px;
   right: 10px;
@@ -329,7 +374,7 @@ const Descript=styled.span`
 const RemainTimeDiv = styled.div`
     box-sizing: border-box;
     width: 100%;
-    height: 10vh; 
+    height: 15dvh; 
     background-color: #2C2C2E;
     bottom: 0;
     flex-shrink: 0; 
@@ -365,7 +410,7 @@ const RemainTime = styled.span`
 
 const CurrentPrice = styled.span`
     font-size: 90%; 
-    margin-top: 15px;
+    margin-top: 10px;
 `;
 const BottomSecondBox =styled.div`
     height: 100%;
@@ -384,3 +429,39 @@ const BuyButton=styled.button`
     font-size: 80%;
 
 `
+const StyledModal = styled(Modal)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  outline: none;
+  
+`;
+
+const ModalContent = styled.div`
+  background-color: #1C1C1E; 
+  padding-top:5%;
+  width: 100%;
+  max-width: 480px;
+  height: 100dvh;
+  object-fit: contain;
+`;
+
+const CloseButton = styled(Cancle)`
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  margin-bottom: 20px;
+`;
+
+const ModalImgBox=styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  
+`
+const ModalImg=styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover; 
+  `
